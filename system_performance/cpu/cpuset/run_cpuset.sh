7005a4de7a9477d4f74b5a752edd1b0d8e6e771b
twlklove <twlklove@163.com>
2021-08-29 03:42:11 -0400

    add run_cpuset.sh

--- system_performance/cpu/cpuset/run_cpuset.sh
+++ system_performance/cpu/cpuset/run_cpuset.sh
@@ -5,8 +5,8 @@ mount -t cpuset cpuset cpuset
 cd cpuset/
 mkdir prodset
 cd prodset/
-echo 4-7 > cpuset.cpus
+echo 4-7 > cpuset.cpus # cpu list
 echo 1 > cpuset.cpu_exclusive
-echo 0 > cpuset.mems
-echo 36764 > tasks
+echo 0 > cpuset.mems   # mem node
+echo 36764 > tasks # pid
 
