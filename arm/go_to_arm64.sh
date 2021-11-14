ca06deba868747de0ad4cc3fe30fe1c86765e868
twlklove <twlklove@163.com>
2021-11-14 02:40:51 -0500

    modify go

--- arm/go_to_arm64.sh
+++ arm/go_to_arm64.sh
@@ -1,2 +1,2 @@
 TMOUT=0
-ssh -t 192.168.112.129 "TMOUT=0; cd /home/i_work/qemu/; ./start_qemu.sh linux-5.12.4"
+ssh -t 192.168.112.129 "TMOUT=0; pwd; ls /home/i_work; cd /home/i_work/qemu; ./start_qemu.sh linux-5.12.4"
