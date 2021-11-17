8c5314310ae7f369d9cfa55b0fbe64c39fe980b4
twlklove <twlklove@qq.com>
2021-11-17 02:05:49 +0000

    modify

--- arm/install.sh
+++ arm/install.sh
@@ -1,19 +1,14 @@
-cd qume_install/
-./install_linux_tool.sh
-cd -
 
 cd arm_cross_compile/arm_compile_tool
-download.sh
+./download.sh
 cd -
 
-cd uboot/
-download.sh
-run.sh
-cd -
-
-cd qume_install/
+pushd qume_install/
+./install_linux_tool.sh
 ./install_qemu.sh
 ./make_linux.sh
+pushd rootfs
 ./create_lib.sh  
 ./mkrootfs.sh
-cd -
+popd
+popd
--- arm/qume_install/install_qemu.sh
+++ arm/qume_install/install_qemu.sh
@@ -6,9 +6,9 @@ wget https://busybox.net/downloads/busybox-1.33.1.tar.bz2
 cp busybox-1.33.1.tar.bz2 rootfs
 cd rootfs 
 tar xvf busybox-1.33.1.tar.bz2
-mkrootfs.sh
+./mkrootfs.sh
 cd -
 
 wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-5.12.4.tar.xz
-tar xvf linux-5.12.4.tar.x
+tar xvf linux-5.12.4.tar.xz
 
