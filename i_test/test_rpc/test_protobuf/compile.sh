0e5b89600d3e736c37574114d1d0c96d52703bbf
twlklove <twlklove@qq.com>
2021-11-22 05:35:29 +0000

    xx

--- i_test/test_rpc/test_protobuf/compile.sh
+++ i_test/test_rpc/test_protobuf/compile.sh
@@ -1,4 +1,4 @@
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/root/.local/lib/pkgconfig
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/lib/x86_64-linux-gnu/pkgconfig:/root/.local/lib/pkgconfig
 export PATH=$PATH:/root/.local/bin
 make clean
 make -j8
