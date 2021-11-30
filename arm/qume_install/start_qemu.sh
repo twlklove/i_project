d70bfc0dec31e2e7da0bf5bc78bcc154fb386358
twlklove <twlklove@qq.com>
2021-11-30 08:34:27 +0000

    xx

--- arm/qume_install/start_qemu.sh
+++ arm/qume_install/start_qemu.sh
@@ -19,7 +19,7 @@ then
     	--append "noinitrd root=/dev/vda rw console=ttyAMA0 loglevel=8" -nographic \
     	-drive if=none,file=${rootfs_file},id=hd0 -device virtio-blk-device,drive=hd0 \
     	--fsdev local,id=kmod_dev,path=$PWD/kmodules,security_model=none \
-    	-device virtio-9p-device,fsdev=kmod_dev,mount_tag=kmod_mount
+    	-device virtio-9p-device,fsdev=kmod_dev,mount_tag=kmod_mount -S -s
 
 else   
     qemu-system-aarch64 -machine virt -cpu cortex-a57 -machine type=virt -m 1024 -smp 4 -kernel arch/arm64/boot/Image \
