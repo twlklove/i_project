3d6f45113d1d3314905cc2ecb7a3c11484564a93
twlklove <twlklove@qq.com>
2022-08-18 09:04:10 +0000

    modify calc.py

--- i_tools/calc.py
+++ i_tools/calc.py
@@ -6,12 +6,20 @@ step = 0.01
 max_step_count = 13
 max_year = 10
 width = 6
-argc = len(sys.argv)
-if argc > 1 :
-    max_step_count = int(sys.argv[1])
 
-if argc > 2 :
-    max_year = int(sys.argv[2])
+base_value_index = 1
+max_step_count_index = 2
+max_year_index = 3
+
+argc = len(sys.argv)
+if argc > base_value_index :
+    base_value = float(sys.argv[base_value_index])
+
+if argc > max_step_count_index :
+    max_step_count = int(sys.argv[max_step_count_index])
+
+if argc > max_year_index :
+    max_year = int(sys.argv[max_year_index])
 
 for i in range(max_year):
     print('%6d' %(i+1), end=' ')
