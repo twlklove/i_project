e56ec922d1479d6f3f224dfa3497da409e7eb519
twlklove <twlklove@qq.com>
2022-11-10 03:52:54 +0000

    modfiy test_packet

--- i_test/tcp_ip/test_packet.cpp
+++ i_test/tcp_ip/test_packet.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <getopt.h>
 #include <csignal>
 
 #include <pthread.h>
@@ -40,23 +41,31 @@
 
 #define ETH   ((char*)("ens33"))
 
+/* etherner_packet = 14B ether_hdr + protocol_data + 4B CRC */
+#define ETH_MIN_FRMAE_LEN  42           //   42B: 14B ether_hdr_len + 28Barp_hdr_len
+#define ETH_MAX_FRAME_LEN  ETH_FRAME_LEN  // 1514B: 14B ether_hdr_len + 1500B MTU
+
 #ifndef dump
-#define dump(a, ...)            printf("%s[%d] mark: " a, __FILE__, __LINE__, ##__VA_ARGS__)
+#define dump(a, ...)                printf("%s[%d] mark: " a, __FILE__, __LINE__, ##__VA_ARGS__)
 #endif
 
-#define debug 1
+#define DEFAULT_DUMP_LEN   80
 
-#if debug
+#define DEBUG 1
+
+#if DEBUG
 #undef  dump_debug 
 #define dump_debug(a, ...)      dump(a, ##__VA_ARGS__)
 #define output_data             printf
 #define dump_data(p_data, len)                                             \
 ({                                                                         \
     s32 i =0;                                                              \
-    len = len > 60 ? 60 : len;                                             \
+    output_data("\nlen is %d\n", len);                                     \
+    len = len > cfg_dump_len ? cfg_dump_len : len;                         \
     for(i = 0; i < len; i++) {                                             \
-        if ((0 != i) && (0 == i % 16)) {                                   \
+        if (0 == i % 16) {                                                 \
             output_data("\n");                                             \
+	    output_data("0x%04x: ", i);                                    \
         }                                                                  \
                                                                            \
         if (i + 1 < len) {                                                 \
@@ -67,7 +76,7 @@
             output_data("%02x", p_data[i]&0xFF);                           \
         }                                                                  \
     }                                                                      \
-    output_data("\n\n");                                                   \
+    output_data("\n");                                                     \
 })
 #else
 #define dump_debug(a, ...) 
@@ -227,6 +236,9 @@ s32 modify_eth_flag(s32 fd, char *p_eth_name, u8 cmd, u32 flag)
 }
 
 int recv_thread_running = 1;
+char *cfg_p_eth_name = ETH;
+int cfg_protocol = 0;
+int cfg_dump_len = DEFAULT_DUMP_LEN;
 
 void *recv_thread(void *p_args)
 {
@@ -244,7 +256,7 @@ void *recv_thread(void *p_args)
         }
 
 	
-        ret = modify_eth_flag(fd, ETH, OPEN_ETH_FLAG, IFF_PROMISC);
+        ret = modify_eth_flag(fd, cfg_p_eth_name, OPEN_ETH_FLAG, IFF_PROMISC);
 	if (0 != ret) {
             dump("fail to open promisc\n");
 	    break;
@@ -253,7 +265,7 @@ void *recv_thread(void *p_args)
 	struct sockaddr addr;
 	bzero(&addr, sizeof(addr));
 	addr.sa_family = PF_PACKET;
-	strncpy(addr.sa_data, ETH, sizeof(addr.sa_data)-1);
+	strncpy(addr.sa_data, cfg_p_eth_name, sizeof(addr.sa_data)-1);
 	ret = bind(fd, &addr, sizeof(addr));
 	if (0 != ret) {
 	    dump("fail to bind\n");
@@ -279,10 +291,13 @@ void *recv_thread(void *p_args)
 	memset(events, 0, sizeof(events));
 	struct sockaddr remote_addr;
 	bzero(&remote_addr, sizeof(remote_addr));
-	strncpy(addr.sa_data, ETH, sizeof(addr.sa_data) - 1);
+	strncpy(remote_addr.sa_data, cfg_p_eth_name, sizeof(remote_addr.sa_data) - 1);
 	socklen_t remote_addr_len = sizeof(remote_addr);
-	u32 max_data_size = ETH_FRAME_LEN;
-        u8 buf[max_data_size] = {0};
+
+	/* ETH_FRAME_LEN = 1514; ETH_ZLEN = 60; ETH_CRC_LEN = 4; 
+	   ETHER_MAX_LEN=ETH_FRAME_LEN + ETHER_CRC_LEN, ETHER_MIN_LEN=ETH_ZLEN + ETHER_CRC_LEN, defined in net/ethernet.h*/
+	u32 max_data_size = ETH_FRAME_LEN;  
+	u8 buf[max_data_size] = {0};
 
 	while (recv_thread_running) {
 	    s32 fds = epoll_wait(epfd, events, max_events, 200);
@@ -299,14 +314,18 @@ void *recv_thread(void *p_args)
 	                continue;
 		    }
 
-	    	    ssize_t len = recvfrom(fd_tmp, buf, max_data_size, 0, &remote_addr, &remote_addr_len);
-	    	    dump_data(buf, len);
+	    	    s32 len = recvfrom(fd_tmp, buf, max_data_size, 0, &remote_addr, &remote_addr_len);
+		    if ((len < ETH_MIN_FRMAE_LEN) || (len > ETH_MAX_FRAME_LEN)) {
+			dump("len is wrong : %d\n", len);
+		    }
+
+		    dump_data(buf, len);
 	        }
 	    }
         }
     } while(0);
 
-    ret = modify_eth_flag(fd, ETH, CLOSE_ETH_FLAG, IFF_PROMISC);
+    ret = modify_eth_flag(fd, cfg_p_eth_name, CLOSE_ETH_FLAG, IFF_PROMISC);
     if (0 != ret) {
         dump("fail to close promisc\n");
     }
@@ -329,28 +348,100 @@ void signal_handler( int signum )
     recv_thread_running = 0; 
 }
 
+void help()
+{
+    dump("help\n");
+}
+
+s32 parse_args(s32 argc, char *argv[])
+{
+    s32 ret = 0;
+    char *short_opts = (char*)"i:abudtl::h";
+    struct option long_opts[] = {
+	{"eth",       required_argument, NULL, 'i'},
+        {"arp",       no_argument,       NULL, 'a'},
+	{"broadcast", no_argument,       NULL, 'b'},
+        {"udp",       no_argument,       NULL, 'u'},
+        {"dhcp",      no_argument,       NULL, 'd'},
+        {"tcp",       no_argument,       NULL, 't'},
+	{"dump_len",  optional_argument, NULL, 'l'},
+	{"help",       no_argument,      NULL, 'h'},
+        {0, 0, 0, 0},
+    };
+
+    int opt = 0;;
+    int opt_index = 0;
+    while((opt = getopt_long(argc, argv, short_opts, long_opts, &opt_index)) != -1){
+        switch(opt) {
+            case 'i':
+                dump_debug("%s\n", optarg);
+		cfg_p_eth_name = optarg;          // optarg is argv[optind-1]
+                break;
+	    case 'a':
+            case 'b':
+            case 'u':
+	    case 'd':
+	    case 't':
+		dump_debug("%s\n", argv[optind-1]);
+                cfg_protocol = atoi(argv[optind-1]);
+                break;
+	    case 'l':
+		dump_debug("%s\n", argv[optind - 1]);
+		dump_debug("%s\n", argv[optind]);
+		cfg_dump_len = atoi(argv[optind]);
+                break;
+	    case 'h':
+		help();
+                exit(0);
+
+	    default:
+		help();
+		ret = -1;
+		return ret;
+        }
+    }
+
+    if (optind > argc) {
+        ret = -1;
+    }
+
+    return ret;
+}
+
 s32 main(s32 argc, s8 *argv[])
 {
-    pthread_t ptd;
+    pthread_t ptd = 0;
     void *p_args = NULL;
     s32 ret = 0;
 
-    int signum = SIGINT; //SIGTERM;//SIGINT;
-    signal(signum, signal_handler);
-
     do {
+	ret = parse_args(argc, argv);
+        if (0 != ret) {
+            dump("fail to parse args\n");
+            break;
+        }
+
+	int signum = SIGINT; //SIGTERM;//SIGINT;
+        sighandler_t sighandler = signal(signum, signal_handler);
+        if (SIG_ERR == sighandler) {
+	    dump("fail to signal\n");
+	    ret = -1;
+	    break;
+        }
+
         ret = pthread_create(&ptd, NULL, recv_thread, p_args);
         if (0 != ret) {
             dump("fail to create thread\n");
 	    break;
         }
 
-        pthread_setname_np(ptd, "recv_packet_thread");
-        if (0 != ptd) {
-            pthread_join(ptd, NULL);
-	    ptd = 0;
-        }
+        pthread_setname_np(ptd, "recv_packet_thread"); 
     } while (0);
+
+    if (0 != ptd) {
+        pthread_join(ptd, NULL);
+        ptd = 0;
+    }
 
     return ret;
 }
