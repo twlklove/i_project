28c7898375ee42576dd9f4eeb413726065c9a0b3
twlklove <twlklove@qq.com>
2022-12-30 08:39:50 +0000

    modify git_lookup

--- git_lookup.sh
+++ git_lookup.sh
@@ -1,5 +1,5 @@
 file=i_document/linux_readme
-content="linux" #func_name, key_word and so on
+content="linux"  #""  #func_name, key_word and so on
 
 if [ $# -eq 2 ]
 then
@@ -11,11 +11,12 @@ fi
 pattern="\(commit\|Author:\|Date:\|${file}\)"
 git log --stat | grep ${file} -n20 | grep ${pattern} |grep -v " commit" | sed 's/commit/\r\ncommit/g' | grep ${file} -B 4 --color=always
 
+commit_num=4
 buf=(`git log --stat | grep ${file} -n20 | grep ${pattern} |grep -v " commit" | sed 's/commit/\r\ncommit/g' | grep ${file} -B 4 \
-     | grep commit | cut -d' ' -f2`)
+     | grep commit | cut -d' ' -f2 | head -n ${commit_num}`)
 
 num=${#buf[*]}
-i=${num}; j=${num}; show_line=3
+i=${num}; j=${num}; show_line=10
 while [ 1 ]
 do    
     let i=$i-1
@@ -37,8 +38,15 @@ do
     printf "%3s%3s : %30s  %30s\n" ${i}, ${j} ${commit_i} ${commit_j}
 
     #git diff commit^!, or git show commit, or git diff commit1 commit2 path
-    git diff ${commit_i} ${commit_j} ${file} | grep ${content} -C ${show_line} --color=always
+
+    if [ "Y"${content} == "Y" ]
+    then
+        git diff --color ${commit_i} ${commit_j} ${file} | xargs -i -n 1 echo {}
+    else
+        git diff --color ${commit_i} ${commit_j} ${file} | grep ${content} -C ${show_line} --color=always
+    fi
    
+    echo
     echo
 done
  
