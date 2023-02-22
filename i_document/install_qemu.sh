28600905c842d32b5737e69f3686df1a5e554adc
twlklove <twlklove@163.com>
2023-02-22 11:52:39 +0800

    xx

---
+++ i_document/install_qemu.sh
@@ -0,0 +1,16 @@
+install_cmd="apt-get install"
+
+### install
+${install_cmd}  qemu-system-arm
+
+#git clone https://gitlab.com/qemu-project/qemu.git
+#cd qemu
+#git submodule init
+#git submodule update --recursive
+##./configure --help|less
+##./configure
+##apt-get install ninja-build
+#./configure --target-list=arm-softmmu,aarch64-softmmu --enable-debug --enable-sdl
+#make
+#make install
+
