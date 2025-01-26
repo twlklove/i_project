6947a208f7e98fcdccc3d16c8b3d2323ea154835
twlklove <twlklove@xx.com>
2025-01-26 11:41:02 +0800

    xx

--- ai/pytorch/d2l.py
+++ ai/pytorch/d2l.py
@@ -1665,37 +1665,6 @@ def test_transformer():
         translation, dec_attention_weight_seq = predict_seq2seq(net, eng, src_vocab, tgt_vocab, num_steps, device, True)
         print(f'{eng} => {translation}, ',  f'bleu {bleu(translation, fra, k=2):.3f}')
 
-
-import seaborn as sns
-import math
-def get_positional_encoding(max_seq_len, embed_dim):
-    # 初始化一个positional encoding
-    # embed_dim: 字嵌入的维度
-    # max_seq_len: 最大的序列长度
-    positional_encoding = np.array([
-        [pos / np.power(10000, 2 * i / embed_dim) for i in range(embed_dim)]
-        if pos != 0 else np.zeros(embed_dim) for pos in range(max_seq_len)])
-    
-    positional_encoding[1:, 0::2] = np.sin(positional_encoding[1:, 0::2])  # dim 2i 偶数
-    positional_encoding[1:, 1::2] = np.cos(positional_encoding[1:, 1::2])  # dim 2i+1 奇数
-    return positional_encoding
-
-def test_7():
-    positional_encoding = get_positional_encoding(max_seq_len=100, embed_dim=16)
-    plt.figure(figsize=(10,10))
-    sns.heatmap(positional_encoding)
-    plt.title("Sinusoidal Function")
-    plt.xlabel("hidden dimension")
-    plt.ylabel("sequence length")
-
-    plt.figure(figsize=(8, 5))
-    plt.plot(positional_encoding[1:, 1], label="dimension 1")
-    plt.plot(positional_encoding[1:, 2], label="dimension 2")
-    plt.plot(positional_encoding[1:, 3], label="dimension 3")
-    plt.legend()
-    plt.xlabel("Sequence length")
-    plt.ylabel("Period of Positional Encoding")
-    
 if __name__ == '__main__':
     #test_0()
    # test_1()
