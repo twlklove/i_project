92f91738c4c5016c8ddfae717eedd87ca2fb3e57
twlklove <twlklove@163.com>
2026-03-07 11:43:18 +0800

    ..

--- i_document/install_linux_machine.sh
+++ i_document/install_linux_machine.sh
@@ -1,16 +1,19 @@
-#sudo passwd root
-#su root
-#/etc/sshd_config PermitRootxx 
-#apt remove xx
-#apt-cache search xx
+## for root
+#sudo passwd
+#sudo -
 
-apt install -y linux-source
-apt install -y linux-headers-$(uname -r)
-
-apt install -y net-tools
-apt install -y sshd
 apt install -y vim
 
+## for openssh
+apt install -y openssh-server
+#set PermitRootLogin yes 
+vim /etc/ssh/sshd_config 
+service ssh starting
+
+######################
+apt install -y git
+apt install -y linux-source
+apt install -y linux-headers-$(uname -r)
 apt install -y gcc
 apt install -y g++
 apt install -y cmake
@@ -18,17 +21,9 @@ apt install -y build-essential autoconf libtool pkg-config
 apt install -y libssl-dev
 apt install -y libncurses-dev
 apt install -y libncurses5
-apt install -y libpython2.7
+apt install -y net-tools
 apt install -y valgrind 
 
 apt install -y w3m w3m-img  #brower
 apt install -y curl
 apt install -y wget
-apt install -y git
-
-apt install -y python3-pip
-
-pip3 install numpy
-pip3 install pandas
-pip3 install matplotlib
-
